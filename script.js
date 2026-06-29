47b385d84b45cd5ed6840c40042477f7c21cc89e
1F10260202 <s1f102602026@iniad.org>
2026-06-29 17:53:09 +0900

    Add new image, update HTML structure, enhance CSS styles, and refactor JavaScript functions
    - Added a new image file for enhanced visual content.
    - Removed unnecessary preconnect links in index.html for improved performance.
    - Wrapped sidebar title in a new div for better styling control.
    - Updated character image class for consistent styling.
    - Revised text for clarity and improved user experience.
    - Adjusted video element dimensions in movie.html for better layout.
    - Refactored JavaScript functions to streamline section opening and closing logic.
    - Enhanced CSS styles for better layout, including sidebar and attention sections.
    - Improved overall design consistency and readability across the site.

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-function removeOpen() {
-    document.querySelectorAll(".page").forEach(page => {
-        page.classList.remove("open");
-    });
-}
-
-function closeAccordionMenu() {
-    const menu = document.querySelector(".accordion-menu");
-    const arrow = document.querySelector(".sidebar-btn .material-symbols-outlined");
-
-    menu.classList.remove("open");
-    arrow.classList.remove("rotate");
-}
-
 function openSection(id,button) {
-    removeOpen();
+    closeSection();
 
     const top = document.getElementById(id);
     top.classList.add("open");
@@ -25,13 +11,10 @@ function openSection(id,button) {
     };
 }
 
-function sidebarButtonActive(button){
-
-    document.querySelectorAll(".sidebar-btn,.accordion-item").forEach(btn=>{
-            btn.classList.remove("active");
-        });
-
-    button.classList.add("active");
+function closeSection() {
+    document.querySelectorAll(".page").forEach(page => {
+        page.classList.remove("open");
+    });
 }
 
 function openAccordionMenu(button){
@@ -40,4 +23,21 @@ function openAccordionMenu(button){
     menu.classList.toggle("open");
 
     button.querySelector(".arrow").classList.toggle("rotate");
+}
+
+function closeAccordionMenu() {
+    const menu = document.querySelector(".accordion-menu");
+    const arrow = document.querySelector(".arrow");
+
+    menu.classList.remove("open");
+    arrow.classList.remove("rotate");
+}
+
+function sidebarButtonActive(button){
+
+    document.querySelectorAll(".sidebar-btn,.accordion-item").forEach(btn=>{
+            btn.classList.remove("active");
+        });
+
+    button.classList.add("active");
 }
