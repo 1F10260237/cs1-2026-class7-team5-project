07293bfe3edac38fd252c3fe510f45605fefb0eb
1F10260202 <s1f102602026@iniad.org>
2026-07-01 23:07:18 +0900

    ギャラリーセクションを改良し、スライダー機能を追加

--- script.js
+++ script.js
@@ -53,3 +53,35 @@ function sidebarButtonActive(button){
 
     button.classList.add("active");
 }
+
+let current = 0;
+function showSlide(index){
+
+    const slides = document.querySelectorAll(".slide");
+    const dots = document.querySelectorAll(".dot");
+
+    slides.forEach(slide => slide.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+}
+
+function changeSlide(num){
+
+    const slides = document.querySelectorAll(".slide");
+    const dots = document.querySelectorAll(".dot");
+
+    current += num;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    if(current < 0){
+        current = slides.length-1;
+    }
+
+    showSlide(current);
+
+}
